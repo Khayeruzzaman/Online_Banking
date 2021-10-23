c8c2c9f1b946935fb209608d5710875df72f6224
ShihabUddinAhmed <shihabahmed8957@gmail.com>
2021-10-23 15:22:46 +0600

    Database Migrations

---
+++ database/migrations/2021_10_23_051744_create_bank_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBankUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bank_users', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname', 20);
+            $table->string('lastname', 20);
+            $table->string('gender', 7);
+            $table->date('dateofbirth');
+            $table->string('phone', 14);
+            $table->string('email', 50);
+            $table->string('userprofilepicture')->nullable();
+            $table->string('nid', 15);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bank_users');
+    }
+}
